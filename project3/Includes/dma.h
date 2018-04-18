e4807c970468a0838c1abf4107cb898d3dd35315
Kcjmagickim <Kcjmagickim@gmail.com>
2018-04-18 00:47:44 -0600

    Update dma.h

--- project3/Includes/dma.h
+++ project3/Includes/dma.h
@@ -1,3 +1,10 @@
+/**********************************
+@file: dma.h
+@brief: header file for dma.c
+@author: John Kim
+@date: Mar 31st, 2018
+***********************************/
+
 #ifndef DMA_H_
 #define DMA_H_
 
@@ -13,11 +20,22 @@
 #define DESTINATION_ADDRESS 0x20000000
 
 void dma_setup(uint8_t mode);
+/***************************************
+@brief: Initializes DMA with interrupt, variable source size, and peripheral request
+@param: mode - selects between three different source size
+****************************************/
 
-uint8_t * memmove_dma(uint8_t *src, uint8_t *dst, size_t length);
+void memmove_dma(uint8_t *src, uint8_t *dst, size_t length);
+/***************************************
+@brief: moves length number of bytes from source to destination
+@param: src-source pointer dst-destination pointer length-number of bytes to transfer
+****************************************/
 
-uint8_t * memset_dma(uint8_t *src, size_t length, uint8_t value);
-
+void memset_dma(uint8_t *src, size_t length, uint8_t value);
+/***************************************
+@brief: sets length number of bytes as value
+@param: src-address to set length-number of bytes to set value-value to set
+****************************************/
 void DMA0_IRQHandler(void);
 
 #endif /*DMA*/
